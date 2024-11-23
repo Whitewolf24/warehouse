3f2adc16cbd0fbf1d3cadd0d05090200113198ef
Whitewolf24 <geo_marinos@yahoo.com>
2024-11-23 06:44:16 +0200

    1.14 minification (working)

--- resources/views/product/index.blade.php
+++ resources/views/product/index.blade.php
@@ -41,9 +41,7 @@
                <p class="fields product-price">{{ $product->price}}</p>
                <p class="fields product-size">{{ $product->size}}</p>
                <p class="fields product-weight">{{ $product->weight}}</p>
-               <p class="fields product-height">{{ $product->height}}</p>
-               <p class="fields product-width">{{ $product->width}}</p>
-               <p class="fields product-length">{{ $product->length}}</p>
+               <p class="fields product-furniture">{{ $product->height}}{{ $product->width}}{{ $product->length}}</p>
             </div>
             @endforeach
             @else
