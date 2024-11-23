fd671cf3b5044de3f1e035aaed1db8ea7dd0c474
Whitewolf24 <geo_marinos@yahoo.com>
2024-11-23 06:08:39 +0200

    1.14 minification (working)

--- resources/views/product/index.blade.php
+++ resources/views/product/index.blade.php
@@ -6,7 +6,7 @@
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <script src="https://code.jquery.com/jquery-3.6.1.min.js" crossorigin="anonymous"></script>
-   <title>Product List</title>
+   <title>Warehouse</title>
    <!--    <link href="https://warehouse-riwu.onrender.com/css/style.css" rel="stylesheet"> -->
 </head>
 
@@ -52,126 +52,5 @@
 </html>
 
 <style>
-   /* Styling some basic elements */
-   *,
-   *::before,
-   *::after {
-      box-sizing: border-box;
-   }
-
-   body,
-   html {
-      border: 0px;
-      font-family: "Open Sans", sans-serif;
-      height: 100vh;
-      margin: 0px;
-      min-height: -webkit-fill-available;
-      min-height: 100vh;
-      padding: 0px;
-   }
-
-   a {
-      list-style: none;
-      text-decoration: none;
-   }
-
-   h1 {
-      height: 0.1em;
-   }
-
-   /* -------------------------------------------- */
-   /* Styling the header and its elements */
-   header {
-      align-content: center;
-      border-bottom: solid rgb(100, 100, 100) 2px;
-      padding-bottom: 0.5em;
-      margin-top: 2em;
-      left: 2em;
-      position: relative;
-      width: 92%;
-   }
-
-   .buttons button {
-      border: solid black 2px;
-      bottom: 2.6em;
-      display: block;
-      filter: drop-shadow(2px 2px 2px #000);
-      float: right;
-      padding-block: 5px;
-      padding-inline: 10px;
-      position: relative;
-   }
-
-   header h1 {
-      margin-bottom: 1.3em;
-   }
-
-   #add-product-btn {
-      right: 11em;
-   }
-
-   #delete-product-btn {
-      left: 3.5em;
-   }
-
-   #save-product-btn {
-      right: 8em;
-   }
-
-   #cancel-btn {
-      left: 3.7em;
-   }
-
-   /* -------------------------------------------- */
-   /* Styling the main content */
-
-   /* First page */
-
-   .alert-success {
-      text-align: center;
-      margin-top: 2em;
-      color: green;
-   }
-
-   .alert-error {
-      text-align: center;
-      margin-top: 2em;
-      color: red;
-   }
-
-   #main_content {
-      display: flex;
-      flex-wrap: wrap;
-      gap: 2em 2em;
-      justify-content: center;
-      margin-block: 3em;
-      margin-inline: 13em;
-   }
-
-
-   .box {
-      border: solid black 2px;
-      font-size: max(2.2vmin, 90%);
-      height: 13em;
-      min-height: 13em;
-      min-width: 15em;
-      width: 15em;
-   }
-
-   .delete-checkbox {
-      height: 10%;
-      margin-left: 1em;
-      margin-top: 1.5em;
-      width: 7%;
-   }
-
-   .fields {
-      bottom: 1em;
-      position: relative;
-      text-align: center;
-   }
-
-   .fields p {
-      line-height: 0.6;
-   }
+.box,.buttons button{border:2px solid #000}.buttons button,.fields,header{position:relative}.alert-error,.alert-success,.fields{text-align:center}*,::after,::before{box-sizing:border-box}body,html{border:0;font-family:"Open Sans",sans-serif;height:100vh;margin:0;min-height:100vh;padding:0}a{list-style:none;text-decoration:none}h1{height:.1em}header{align-content:center;border-bottom:2px solid #646464;padding-bottom:.5em;margin-top:2em;left:2em;width:92%}.buttons button{bottom:2.6em;display:block;filter:drop-shadow(2px 2px 2px #000);float:right;padding-block:5px;padding-inline:10px}header h1{margin-bottom:1.3em}#add-product-btn{right:11em}#delete-product-btn{left:3.5em}#save-product-btn{right:8em}#cancel-btn{left:3.7em}.alert-success{margin-top:2em;color:green}.alert-error{margin-top:2em;color:red}#main_content{display:flex;flex-wrap:wrap;gap:2em 2em;justify-content:center;margin-block:3em;margin-inline:13em}.box{font-size:max(2.2vmin, 90%);height:13em;min-height:13em;min-width:15em;width:15em}.delete-checkbox{height:10%;margin-left:1em;margin-top:1.5em;width:7%}.fields{bottom:1em}.fields p{line-height:.6}
 </style>
